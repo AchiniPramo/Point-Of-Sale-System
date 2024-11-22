4c855bba365e2467bc763dd12214795873502aac
AchiniPramo <achinipramodhya4@gmail.com>
2024-11-22 22:15:39 +0530

    update transaction logic components

---
+++ models/order.js
@@ -0,0 +1,17 @@
+function Order(
+  orderId,
+  date,
+  customerId,
+  discount,
+  cash,
+  balance,
+  orderDetails
+) {
+  this.orderId = orderId;
+  this.date = date;
+  this.customerId = customerId;
+  this.discount = discount;
+  this.cash = cash;
+  this.balance = balance;
+  this.orderDetails = orderDetails;
+}
---
+++ util/ScriptLoader.js
@@ -0,0 +1,31 @@
+function loadScript(url, defer = true) {
+  const script = $("<script>", {
+    src: url,
+    defer: defer,
+    async: !defer,
+  });
+  $("head").append(script);
+}
+
+loadScript("/db/DB.js");
+loadScript("/util/CustomAlert.js");
+loadScript("/util/GenerateNextID.js");
+loadScript("/util/Validate.js");
+loadScript("/util/CommonFunctions.js");
+
+// loadScript("/models/Customer.js");
+// loadScript("/models/Order.js");
+// loadScript("/models/OrderDetail.js");
+// loadScript("/models/Item.js");
+// loadScript("/controllers/IndexController.js");
+
+loadScript("/models/customer.js");
+loadScript("/models/order.js");
+loadScript("/models/OrderDetail.js");
+loadScript("/models/item.js");
+loadScript("/controllers/IndexController.js");
+
+loadScript("/controllers/HomeController.js");
+loadScript("/controllers/CustomerController.js");
+loadScript("/controllers/OrderController.js");
+loadScript("/controllers/ItemController.js");
